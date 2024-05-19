435939d05c9acc1d6f13291e09466ed597a399f7
ryan soohoo <ryansoohoo@gmail.com>
2024-05-19 15:08:01 -0700

    spawn pattern

---
+++ Assets/_Scripts/SpawnPattern.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct SpawnInfo
+{
+    public int spawnWave;
+    public Collectable item;
+    public bool hasBeenCollected;
+}
+
+public class SpawnPattern : MonoBehaviour
+{
+    public SpawnInfo[] dictionary;
+    public static SpawnPattern instance;
+
+    public void Awake()
+    {
+        if(instance == null)
+            instance = this;
+    }
+}
