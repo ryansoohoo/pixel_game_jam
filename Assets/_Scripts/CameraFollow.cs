377cee795f8e6eec2c84d72850ab39086f28fd1f
ryan soohoo <ryansoohoo@gmail.com>
2024-05-20 01:00:47 -0700

    stuff

--- Assets/_Scripts/CameraFollow.cs
+++ Assets/_Scripts/CameraFollow.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    public Transform boatPos;
     public Transform follow;
     public float slerpValue = 6f;
     Vector3 newPos;
+    public bool inBoat;
     // Update is called once per frame
     void Update()
     {
+        if (inBoat)
+        {
+            newPos = boatPos.position;
+        }
         newPos = follow.position;
         newPos.z = -10;
         transform.position = newPos;
