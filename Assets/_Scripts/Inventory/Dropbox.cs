8a7db49f67e6bb66f8e3823c65375ea3792bc496
PoggerLro <77075171+PoggerLro@users.noreply.github.com>
2024-05-20 21:15:42 +0900

    added dialogue sfx

--- Assets/_Scripts/Inventory/Dropbox.cs
+++ Assets/_Scripts/Inventory/Dropbox.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FMODUnity;
+using System.CodeDom;
 
 public class Dropbox : Triggerbox
 {
     [SerializeField] private EventReference dropOffSound;
+    [SerializeField] private EventReference dialogueSound;
     public Otterboat otterboat;
     public override void OnEnter()
     {
@@ -14,6 +16,7 @@ public class Dropbox : Triggerbox
         {
             MainManager.instance.MoveItem(Player.instance.inventory.collectables[0],Player.instance.inventory,otterboat.inventory);
             AudioManager.instance.PlayOneShot(dropOffSound, this.transform.position);
+            AudioManager.instance.PlayOneShot(dialogueSound, this.transform.position);
         }
         CameraFollow.instance.inBoat = true;
     }
